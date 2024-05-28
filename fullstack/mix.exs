8844659d3474caaebd142f1556114ff39fc11ecd
phonnz <gonzalezm.alfonso@gmail.com>
2024-05-28 23:39:05 +0200

    Adding Vega_lite

--- fullstack/mix.exs
+++ fullstack/mix.exs
@@ -49,7 +49,8 @@ defmodule Fullstack.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:vega_lite, "~> 0.1.9"}
     ]
   end
 
