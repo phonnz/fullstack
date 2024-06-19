bbe231451762052e3cb7ab19d11c03ad35ee3b17
phonnz <gonzalezm.alfonso@gmail.com>
2024-06-19 12:34:28 +0200

    Drop inspect

--- fullstack/lib/fullstack/financial.ex
+++ fullstack/lib/fullstack/financial.ex
@@ -183,7 +183,6 @@ defmodule Fullstack.Financial do
         customer_id: Customers.random_customer_id(),
         pos_id: random_pos_id()
       }
-      |> IO.inspect()
 
     %Transaction{}
     |> Transaction.changeset(new_trx)
