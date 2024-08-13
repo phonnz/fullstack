8f6f32ea65b93c4ff38ee8b13e10ec5cd3353e12
phonnz <gonzalezm.alfonso@gmail.com>
2024-08-13 21:49:25 +0200

    improve messages style

--- fullstack/lib/fullstack_web/live/chat/chat_live.ex
+++ fullstack/lib/fullstack_web/live/chat/chat_live.ex
@@ -2,44 +2,40 @@ defmodule FullstackWeb.ChatLive do
   use FullstackWeb, :live_view
 
   @impl true
-  def mount(params,   session, socket) do
+  def mount(params, session, socket) do
     if connected?(socket) do
-     FullstackWeb.Endpoint.subscribe("chat") 
+      FullstackWeb.Endpoint.subscribe("chat")
     end
-    tmp_id = Map.get(session, "_csrf_token")
+
+    tmp_id = session |> Map.get("_csrf_token") |> String.downcase() |> String.slice(1..12)
+
     {
       :ok,
       socket
       |> assign(:tmp_id, tmp_id)
       |> assign(:form, to_form(%{"message" => ""}, as: :form))
-      #|> assign(:form, get_form(%{}))
       |> assign(:messages, load_messages()),
-       temporary_assigns: [messages: []]
-      
+      temporary_assigns: [messages: []]
     }
   end
 
   @impl true
   def render(assigns) do
-
     ~H"""
-    <div class="space-y-4"> 
+    <div class="space-y-4">
       <.header>
-        Hooah! This is your identifier: <span class="bg-slate-200 p-2 rounded-sm"><%= String.slice(assigns.tmp_id, 1..6) %>  </span>
+        Hooah! This is your identifier:
+        <span class="bg-slate-200 p-2 rounded-sm"><%= assigns.tmp_id %></span>
       </.header>
       <section phx-update="append" id="messages-list">
-      <p :for={message <- @messages } id={"m-#{message.id}"} class="border p-4 italic rounded-lg">
-        <span class="bg-slate-100"><%= message.from %>: </span><%= message.text  %>
-      </p>
+        <p :for={message <- @messages} id={"m-#{message.id}"} class="border p-4 italic rounded-lg">
+          <span class="bg-slate-100 text-gray p-2"><%= message.from %>:</span> <%= message.text %>
+        </p>
       </section>
-    <section class="">
-      <.simple_form for={@form} phx-submit="save">
-        <.input
-          field={@form[:message]}
-          placeholder="Say something!"
-        />
-
-      </.simple_form>
+      <section class="">
+        <.simple_form for={@form} phx-submit="save">
+          <.input field={@form[:message]} placeholder="Say something!" />
+        </.simple_form>
       </section>
     </div>
     """
@@ -49,16 +45,24 @@ defmodule FullstackWeb.ChatLive do
     {:noreply, assign(socket, messages: [income_message])}
   end
 
-  def handle_event("save", %{"form" => %{"message" => message }} = params, socket) do
-    new_message = %{id: :rand.uniform(100), from: String.slice(socket.assigns.tmp_id, 1..5), text: message}
+  def handle_event("save", %{"form" => %{"message" => message}} = params, socket) do
+    new_message = %{
+      id: :rand.uniform(100),
+      from: socket.assigns.tmp_id,
+      text: message
+    }
+
     FullstackWeb.Endpoint.broadcast("chat", "new_message", new_message)
-    {:noreply, assign(socket, :messages,  [new_message]) |> assign(:form, to_form(%{"message" => ""}, as: :form))
-} 
+
+    {:noreply,
+     assign(socket, :messages, [new_message])
+     |> assign(:form, to_form(%{"message" => ""}, as: :form))}
   end
 
   def load_messages() do
     [%{id: :rand.uniform(100), from: "Fullstack", text: "Hi!"}]
   end
+
   defp get_form(params, action \\ :subimt) do
     params
     |> get_changeset()
@@ -74,5 +78,4 @@ defmodule FullstackWeb.ChatLive do
     |> Ecto.Changeset.cast(params, Map.keys(types))
     |> Ecto.Changeset.validate_required([:question])
   end
-
 end
